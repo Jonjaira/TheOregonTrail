3d3f49bdbebfecbb4183d528b10ce8d57c9209ac
Jessica Vargas <jonjaira@gmail.com>
2018-02-15 11:05:22 -0500

    2/15/2018 The Store Control class and the control package were created. Changes to the item class were commited and the StoreControl tests were created as well. Changes in the Item class were made in order to successfully create the tests, those changes in the prices need to be reverted after the assignment is graded

--- theOregonTrail/src/byui/cit260/OregonTrail/model/Item.java
+++ theOregonTrail/src/byui/cit260/OregonTrail/model/Item.java
@@ -16,24 +16,24 @@ public enum Item {
     //This class is an enum because all of its components are fix and won't be
     //changing throughout the game.
     
-    metalWheel("Metal wheel. Will last longer, but will be heavier. Can be used with both metal and wood wagons", "10"),
-    woodWheel ("Wood wheel. Lighter but breaks easier than the metal wheel. Can be only used with a wood wagon", "10"),
-    Oxen("Healthy, strong Oxen", "20"),
-    Food("Delicious, nutritious meals", "1"),
-    Clothing("You will need appropiate attire", "10"),
-    AmmunitionBox("Best value if you buy a box", "5"),
-    Ammunition("For your hunting necessities", "1"),
-    Axle("Better to be prepared in case of damage", "10"),
-    Tongue("Adittional tongues are recomended as precaution", "10"),
-    FirstAidKit("Restores health to a 100%", "40"),
-    Tools("Need to work to provide for your company? You will need tools", "40"),
-    StarterPackage("Don't complicate your life thinking on what you need. This package has the basics", "200");
+    metalWheel("Metal wheel. Will last longer, but will be heavier. Can be used with both metal and wood wagons", 10.0),
+    woodWheel ("Wood wheel. Lighter but breaks easier than the metal wheel. Can be only used with a wood wagon", -10.0),
+    Oxen("Healthy, strong Oxen", 20.0),
+    Food("Delicious, nutritious meals", 0.20),
+    Clothing("You will need appropiate attire", 10.0),
+    AmmunitionBox("Best value if you buy a box", 5.0),
+    Ammunition("For your hunting necessities", 1.0),
+    Axle("Better to be prepared in case of damage", 10.0),
+    Tongue("Adittional tongues are recomended as precaution", 10.0),
+    FirstAidKit("Restores health to a 100%", 40.0),
+    Tools("Need to work to provide for your company? You will need tools", 400.0),
+    StarterPackage("Don't complicate your life thinking on what you need. This package has the basics", 600.0);
     
     private String description;
-    private String price;
+    private double price;
    
     
-    Item (String description, String price){
+    Item (String description, double price){
         this.description = description;
         this.price = price;
 }
@@ -42,7 +42,7 @@ public enum Item {
         return description;
     }
 
-    public String getPrice() {
+    public double getPrice() {
         return price;
     }
 
